b4d466b0786ae5bd0767718c671c1c08daa570d9
Myra Rozmiarek <11marozmi@gmail.com>
2020-10-15 20:56:28 -0500

    updated js fixed a bug

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function promptUser() {
 
     return inquirer.prompt([
         {
-            type: "list",
+            type: "input",
             name: "Title",
             message: "Enter project title here:",
         },
@@ -35,22 +35,15 @@ function promptUser() {
             type: "input",
             name: "usage",
             message: "Provide usage information here:",
-          },
-         { 
-          type: "list",
-        name: "license",
-        message: "Choose a license for your project, from the following list:",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-    },
-    {
-        // {
-        //     type: "list",
-        //     name: "license",
-        //     message: "What are your options?",
-        //     choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-        // },
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What are your options?",
+            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        },
+        {
 
-        
             type: "input",
             name: "contributing",
             message: "Provide contributing guidelines here:"
@@ -78,46 +71,90 @@ function promptUser() {
     ]);
 }
 
-function generateHTML(answers) 
-{
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-      <title>Document</title>
-    </head>
-    <body>
-      <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-        <p class="lead">I am from ${answers.location}.</p>
-        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-        <ul class="list-group">
-          <li class="list-group-item">My GitHub username is ${answers.github}</li>
-          <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-        </ul>
-      </div>
-    </div>
-    </body>
-    </html>`;
-    }
-    //1. prompt questions
-    promptUser()
+ inquirer.prompt(questions).then(answers => {
+  console.log(answer)
+    const data, 
+    
+//    ${answer.badge} 
+    
+//     src=("https://img.shields.io/badge/${answers.badge}-v1.0-green}")
+    // `
+// })
+// function generateHTML(answers) {
+//     return `
+//   ## ${answers.location}
+
+
+//   Sometimes it's useful to have different levels of headings to structure your documents. Start lines with a to create headings. Multiple  in a row denote smaller heading sizes.
+  
+//   ### This is a third-tier heading
+  
+//   You can use one # all the way up to ###### six for different heading sizes.
+  
+//   If you'd like to quote someone, use the > character before the line:
+  
+//   > Coffee. The finest organic suspension ever devised... I beat the Borg with it.
+//   > - Captain Janeway
+// `
+// }
+// 1. prompt questions
+promptUser()
     //2. get data from questions (answers obj)
-      .then(function(answers) {
+    .then(function (answers) {
         //3. use the questions asked and plug into the generatehtml function
         const html = generateHTML(answers);
         //4. replace all variables with the answer object
         //5. template is done, now to create html file
-        return writeFileAsync("index.html", html);
-      })
-      .then(function() {
+        return writeFileAsync("README.md", html);
+    })
+    .then(function () {
         console.log("Successfully wrote to index.html");
-      })
-      .catch(function(err) {
+    })
+    .catch(function (err) {
         console.log(err);
-      });
+    });
+
+
+
+// function generateHTML(answers) {
+//     return  `
+//     <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//       <title>Document</title>
+//     </head>
+//     <body>
+//       <div class="jumbotron jumbotron-fluid">
+//       <div class="container">
+//         <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+//         <p class="lead">I am from ${answers.location}.</p>
+//         <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//         <ul class="list-group">
+//           <li class="list-group-item">My GitHub username is ${answers.github}</li>
+//           <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+//         </ul>
+//       </div>
+//     </div>
+//     </body>
+//     </html>`;
+// }
+// //1. prompt questions
+// promptUser()
+//     //2. get data from questions (answers obj)
+//     .then(function (answers) {
+//         //3. use the questions asked and plug into the generatehtml function
+//         const html = generateHTML(answers);
+//         //4. replace all variables with the answer object
+//         //5. template is done, now to create html file
+//         return writeFileAsync("index.html", html);
+//     })
+//     .then(function () {
+//         console.log("Successfully wrote to index.html");
+//     })
+//     .catch(function (err) {
+//         console.log(err);
+//     });
 
